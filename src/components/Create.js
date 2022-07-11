355d6d9d3f0a727dda0c8a0ed2314dfa7c7d2bd9
KasparasBal <Kasparasbalys@gmail.com>
2022-07-11 14:28:04 +0300

    Commit 5: Added Minor Error Handling & tweaks

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,5 +1,17 @@
+import "../styles/Create.css";
+
 const Create = () => {
-  return <></>;
+  return (
+    <div className="create_container">
+      <form className="create_form">
+        <label className="create_label">Title</label>
+        <input className="create_input" type="text" id="title"></input>
+        <label className="create_label">Body</label>
+        <textarea className="create_input body_input" id="body"></textarea>
+        <button className="create_btn">Create Post</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,29 +7,38 @@ import loading_gif from "../img/Loading_Gif.gif";
 const Home = (props) => {
   const [posts, setPosts] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(true);
+  const [error, setError] = useState(null);
+
+  const errMsg = "";
 
   //Fetch Data from JSON SRV
   useEffect(() => {
     fetch("http://localhost:8000/posts")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("OOPS! something went wrong!");
+        }
         return res.json();
       })
       .then((data) => {
         setPosts(data);
         setLoading(false);
+        setError(null);
       })
-      .catch((err) => {});
+      .catch((err) => {
+        setLoading(false);
+        setError(err.message);
+      });
   }, []);
 
   return (
     <div className="home">
+      {error && <div className="error"> {error} </div>}
       {loading && (
         <div className="gif_container">
           <img className="loading_gif" src={loading_gif} alt="loading..." />
         </div>
       )}
-
       {posts && <Posts posts={posts} />}
     </div>
   );
